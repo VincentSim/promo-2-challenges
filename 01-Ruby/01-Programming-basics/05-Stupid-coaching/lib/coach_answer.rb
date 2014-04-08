af7e8af39551a3772ee4eb6f3f0f0eb8c6d95368
Vincent Simon <vincent.simon@gadz.org>
2014-04-08 16:23:46 +0200

    First attempt, solved it

--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/coach_answer.rb
@@ -1,4 +1,15 @@
 def coach_answer(your_message)
+
+  if your_message.include?("?")
+    "Silly question, get dressed and go to work !"
+
+    elsif your_message =="I am going to work right now SIR !"
+    return ""
+  else
+    "I don't care son, get dressed and go to work !"
+
+
+  end
   # TODO: return coach answer to your_message
 end
 
--- 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/05-Stupid-coaching/lib/interface.rb
@@ -1,3 +1,6 @@
 require_relative "coach_answer"
 #TODO: Implement the program that makes you discuss with your coach from the terminal.
+print "Quelque chose à dire?"
+your_message = gets.chomp
 
+print coach_answer(your_message)
