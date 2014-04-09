155e1ae829a7d89682f5be7d06b0e994c5ad1c7c
Vincent Simon <vincent.simon@gadz.org>
2014-04-09 15:23:35 +0200

    First attempt, solved it

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -2,18 +2,28 @@ require_relative "wagon_sort"
 
 students = []
 
-name = gets.chomp
-
-until name == ""
+begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
-  students << name
-  name = gets.chomp
-end
+  name = gets
+  name.chomp! if name
+
+  # TODO: Add the name we just gotto the students array
+  students << name if name != ""
+
+end while name != ""
 
 
 wagon = wagon_sort(students)
+dernier_eleve = wagon.delete(wagon.last)
+liste = wagon * ", "
 
-puts "congratulations ! Your wagon has #{wagon.length} students"
+if  wagon_sort(students).length > 1
+puts "Congratulations ! Your wagon has #{wagon_sort(students).length} students :
+#{liste} and #{dernier_eleve}"
+
+else
+  puts "Congratulations ! Your wagon has 1 student : #{dernier_eleve}"
+end
 # printer ts les eleves
 
 
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,8 +1,5 @@
 def wagon_sort(students)
-  unless students == 0
-    students.delete ("")
     students.sort_by{|nom| nom.downcase}
-  end
 
 
   # TODO: return a copy of students, sorted alphabetically
