2813b5e9b166073ff49529db807021197300c6de
Vincent Simon <vincent.simon@gadz.org>
2014-04-09 12:50:18 +0200

    First attempt, solved it

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -6,9 +6,14 @@ begin
   puts students.empty? ? "Type a student name:" : "Type another student name (or press enter to finish):"
   name = gets
   name.chomp! if name
-
+  students << name
   # TODO: Add the name we just gotto the students array
 
 end while name != ""
+
+wagon = wagon_sort(students)
+wagon.each do |eleve| puts "-#{eleve}"
+end
+
 
 # TODO: Call `wagon_sort` method and display the sorted student list
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,5 @@
 def wagon_sort(students)
+  students.sort_by{|nom| nom.downcase}
+
   # TODO: return a copy of students, sorted alphabetically
 end
