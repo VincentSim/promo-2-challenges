980dc79c1bf2fd5a5849105729c34c3d83907473
Vincent Simon <vincent.simon@gadz.org>
2014-04-09 12:04:22 +0200

    First attempt, solved it

--- 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Arrays-Enumerable/01-Simple-looping/lib/simple_looping.rb
@@ -1,15 +1,39 @@
+
+
 def sum_with_while(min,max)
+  sum = 0
+
+  while min <= max
+       sum += min
+       min += 1
+
+  end
+  sum
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a while..end structure
 end
 
 def sum_with_for(min,max)
+  sum = 0
+  for i in min..max
+     sum += i
+  end
+  sum
   #TODO: return the sum of the integers between min and max
   #CONSTRAINT: you should use a for..end structure
 end
 
-def sum_recursive(min,max)
-  #TODO (optional): return the sum of the integers between min and max
-  #CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
+def sum_recursive(min, max)
+  if min > max
+    raise ArgumentError
+  end
+
+  if min == max
+    max
+  else
+    min + sum_recursive(min+1, max)
+  end
 end
+
+
 
