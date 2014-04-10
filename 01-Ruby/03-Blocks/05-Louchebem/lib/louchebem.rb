444380d23b8eb599120e7f793f568aa6e136cc7d
Vincent Simon <vincent.simon@gadz.org>
2014-04-10 16:34:51 +0200

    First attempt, solved it

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,34 @@
 # Encoding: utf-8
 
-def louchebemize(sentence)
+def louchebemize(word)
   #TODO: implement your louchebem translator
+  #constraint: you should pick your final random suffix in ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+#constraint: one-letter word like "a" or "l" should not be translated
+#constraint: word beginning with a vowel should be translated differently. Ex: "atout" may give "latoutoc"
+#enhancement: ideally your program should be able to translate any complicated word, eventually embedding special characters
+# like "!", or "'".
+  if word.length == 1
+    mot_final = word
+  elsif 'aeiouy'.include? (word[0] || word[0].downcase)
+   mot_final = changer_word(word)
+  else
+    lettre_debut = word.slice(0)
+    word.slice!(0)
+    mot = word + lettre_debut
+   mot_final = changer_word(mot)
+  end
+  "#{mot_final}"
 end
+
+def changer_word(x)
+#on tire au sort la terminaison du mot
+terminaison = ["em", "é", "ji", "oc", "ic", "uche", "ès"].shuffle!.last
+#on ajout la terminaison au mot
+"l" + x + terminaison
+end
+
+
+
+
+puts louchebemize("l")
+
