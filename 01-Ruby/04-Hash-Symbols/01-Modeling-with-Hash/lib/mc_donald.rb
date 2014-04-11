46e1f16bc0f7c1fdee3a8556870194f40ccfa7fb
Vincent Simon <vincent.simon@gadz.org>
2014-04-11 11:25:41 +0200

    First attempt, solved it

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,25 @@
+HASH = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400,
+    "Royal Cheese" => 130,
+    "French fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Sprite" => 170
+  }
+
 def poor_calories_counter(burger, side, beverage)
   #TODO: return number of calories for this mcDonald order
+   HASH[burger] + HASH[side] + HASH[beverage]
 end
 
 def calories_counter(*orders)
   #TODO: return number of calories for a less constrained order
+  calories = 0
+  orders.each{|order|  calories += HASH[order]}
+  puts calories
 end
+
+puts poor_calories_counter("Cheese Burger", "French fries", "Coca")
+puts calories_counter("Cheese Burger", "French fries", "Coca", "Cheese Burger", "French fries", "Coca")
