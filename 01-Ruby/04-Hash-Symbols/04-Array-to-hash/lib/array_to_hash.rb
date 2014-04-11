60dc6680a49de5b9d7ed891898d3dd331713bb9a
Vincent Simon <vincent.simon@gadz.org>
2014-04-11 17:04:03 +0200

    ayé ayé

--- 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/04-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,21 @@
+  # TODO: implement the method :)
 def array_to_hash(array)
-  #TODO: implement the method :)
+    hash = Hash.new
+    array.each_with_index do | v , i |
+      if block_given?
+        hash[ yield(i) ] = v
+      else
+          hash[ i ] = v
+      end
+    end
+  hash
 end
+
+
+
+
+
+
+
+
+puts array_to_hash(["pierre", "paul"])
