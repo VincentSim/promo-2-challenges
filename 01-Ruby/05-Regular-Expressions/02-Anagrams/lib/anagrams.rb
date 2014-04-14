5e1c950b2bc0c372b60e534c8a66a742f104b913
Vincent Simon <vincent.simon@gadz.org>
2014-04-14 13:00:52 +0200

    First attempt, solved it

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,18 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+  a_string.downcase.split(//).sort.select{|v| v=~/[a-z]/} == another_string.downcase.split(//).sort.select{|v| v=~/[a-z]/} ? true : false
 end
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+  tableau_1 = a_string.downcase.split(//).select{|v| v=~/[a-z]/}
+  tableau_2 = another_string.downcase.split(//).select{|v| v=~/[a-z]/}
+  hash_1 = Hash.new
+  hash_2 = Hash.new
+
+  tableau_1.each{|x| hash_1[x] = tableau_1.count(x)}
+  tableau_2.each{|x| hash_2[x] = tableau_2.count(x)}
+  hash_1 == hash_2 ? true : false
 end
-  
+
+ puts anagrams_on_steroids?("Monica Lewinsky!", "Nic silky woman")
