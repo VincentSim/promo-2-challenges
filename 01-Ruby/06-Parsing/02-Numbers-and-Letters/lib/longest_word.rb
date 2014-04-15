660acecb0aa5d49187227a25d61e5235bfbab488
Vincent Simon <vincent.simon@gadz.org>
2014-04-15 17:13:17 +0200

    doucement

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -1,10 +1,27 @@
 require 'open-uri'
 require 'json'
 
+
+
+
+
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+grid = []
+alphabet = [*("a".."z")]
+for i in 1 .. grid_size.to_i
+  item = alphabet.shuffle[0]
+  grid << item
+end
+grid
 end
 
-def run_game(attempt, grid, start_time, end_time)
+#def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
-end
+#end
+
+uri = URI.parse("http://api.wordreference.com/0.8/80143/json/enfr/apple").read
+jason = JSON.parse (uri)
+puts term_0 = jason["term0"]["PrincipalTranslations"]["0"]["OriginalTerm"]["term"]
+
+
