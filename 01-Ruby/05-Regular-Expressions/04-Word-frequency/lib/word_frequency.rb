9ba8f6b54363069564b7adc8bcf66360f5a0f5a8
Vincent Simon <vincent.simon@gadz.org>
2014-04-15 10:56:21 +0200

    First attempt, solved it

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,13 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
   #TODO: return hash of occurences of number_of_word most frequent words
+  #most_common_words('source-text.txt', 3)
+    #File.open('lib/source-text.txt', "r").each_line do |line|
+  #puts line
+#line.inject(Hash.new(0)) {|hash,word| hash[word] += 1; hash }
 end
+tableau = []
+ File.open('lib/source-text.txt', "r").each_line do |line|
+   #line.inject(Hash.new(0)) {|hash,word| hash[word] += 1; hash }
+   tableau = line.split
+end
+tableau.each_with_object(Hash.new(0)) {|word, hash| hash[word] += 1; hash}
