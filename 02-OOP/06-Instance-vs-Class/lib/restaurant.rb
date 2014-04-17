1b2547ded05ef7b6c27e0714264defd98b1d3204
Vincent Simon <vincent.simon@gadz.org>
2014-04-17 17:23:20 +0200

    First attempt, solved it

--- 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/06-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,26 @@
 class Restaurant
-  
   #TODO add relevant accessors if necessary
-  
+
+  attr_reader :average_rating, :city, :name
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = 0
+    @name_rate = []
   end
-  
+
   #TODO: implement #filter_by_city and #rate methods
-  
+
+  def self.filter_by_city(arg=[], city)
+    result = []
+    result << arg.pop {|resto| resto if resto.city == city}
+  end
+
+
+  def rate(x)
+    @name_rate << x
+    @average_rating = @name_rate.reduce(:+)/@name_rate.length
+  end
 end
