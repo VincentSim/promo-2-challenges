a9fc500a1f4814dd47b6c5e121040e7f46fc352a
Vincent Simon <vincent.simon@gadz.org>
2014-04-28 14:20:58 +0200

    First attempt, solved it

--- 03-AR-Database/07-Hackernews-AR-basics/lib/interface.rb
+++ 03-AR-Database/07-Hackernews-AR-basics/lib/interface.rb
@@ -13,9 +13,9 @@ while true
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
-  
+
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
@@ -23,12 +23,21 @@ while true
     rating = ask_and_get("rating")
     post = { name: name, source_url: source_url, date: Time.now, rating: rating }
     #TODO: use ActiveRecord to add a new entry to the DB
+    #soit on passe en:
+    Post.create!(post)
+    #soit on passe en:
+    #variable = Post.new(post)
+    #variable.save!
+
   when 2
     #TODO: use ActiveRecord to get all entries from the DB
+    Post.all.each{|post| puts "Name: #{post.name}, Source: #{post.source_url}, date: #{post.date}, rating: #{post.rating}"}
+
   when 3
     #TODO: use ActiveRecord to delete all entries from the DB
-  when 4 
+    Post.delete_all
+  when 4
     break
-	end 
-	
+	end
+
 end
