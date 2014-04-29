1bc14dc8e3d383201c65c24af735f9e83b656ed7
Vincent Simon <vincent.simon@gadz.org>
2014-04-29 11:26:06 +0200

    First attempt, solved it

--- 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/db/seed.rb
@@ -2,11 +2,34 @@ require_relative "../config/application"
 require_relative "../models/post"
 require_relative "../models/user"
 
+
 # Seed you database with post data from hacker news
 # And fake users using Faker gem
 
 puts "Seeding database..."
 
-#TODO: Your code goes here 
+#TODO: Your code goes here
 # 1. create fake users
 # 2. Scrape news from https://news.ycombinator.com/ and associate posts to your existing users
+
+
+20.times do
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  User.create!({name: name , email: email})
+end
+
+doc = Nokogiri::HTML(open('https://news.ycombinator.com'))
+
+
+doc.search('table table tr td.title a').each do |element|
+
+  user_id = 1 + rand(20)
+
+  name = element.inner_text
+  datetime = Date.new
+  source_url = element['href']
+  rating = 1 + rand(30)
+  user = User.find(user_id)
+  Post.create!({name: name, source_url: source_url, rating: rating, user: user})
+end
--- 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
+++ 03-AR-Database/09-Hackernews-seeding/lib/interface.rb
@@ -3,3 +3,49 @@ require_relative 'models/post'
 require_relative 'models/user'
 
 #TODO: build your interface with login and CRUD tasks on the posts
+
+def ask_and_get(param)
+  puts "What is the #{param} of your piece of news?"
+  gets.chomp
+end
+
+while true
+
+  puts "Please login with your <id>"
+  #TODO: instantiate a user with his <id>
+  logID = gets.chomp
+  begin
+    user = User.find(logID)
+  rescue
+    puts "Name please"
+    name = gets.chomp
+    puts "Address please"
+    adress = gets.chomp
+    user = User.create!({name: name, email: adress})
+  end
+
+  puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
+  puts "1. Create a post"
+  puts "2. Read your posts"
+  puts "3. Delete all posts"
+  puts "4. Exit"
+
+  choice =  gets.chomp.to_i
+
+  case choice
+  when 1
+    name = ask_and_get("name")
+    source_url = ask_and_get("source url")
+    rating = ask_and_get("rating")
+    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
+    #TODO: use ActiveRecord to add a new post for the user logged in!
+  when 2
+    #TODO: use ActiveRecord to get all posts of the current user
+
+  when 3
+    #TODO: use ActiveRecord to delete all posts of current user
+  when 4
+    break
+  end
+
+end
