f7fed995d5be4df39a642e37e08f54710adcba0e
Vincent Simon <vincent.simon@gadz.org>
2014-05-05 22:56:45 +0200

    j arrive pas reduire lopacite

---
+++ 04-Front-End/04-Javascript-jQuery/03-Modal/application.js
@@ -0,0 +1,8 @@
+
+$('img').click(function() {
+    $("body").append('<div class="back"></div>');
+    //<div><img class= "photo2" src= "image/image1.jpeg"></div>
+    $("div").append('<div><img class= "photo2" src= "image/image1.jpeg"></div>');
+    //$("body").append('<div class="container"><div class = "row"><img class="col-md-12" class= "photo2" src= "image/image1.jpeg"></div></div>');
+});
+//<div><img class= "photo2" src= "image/image1.jpeg"></div>
