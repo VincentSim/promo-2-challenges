5f3ab5761db195ac2b8aae2a858889a681ae567b
Vincent Simon <vincent.simon@gadz.org>
2014-05-05 18:33:06 +0200

    avé César, respect robustesse

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,22 @@
   * TODO: add your code here!
   *
 */
+
+$("#message-wrapper").hide();
+$("#message-wrapper").slideDown().delay(1500);
+
+$('#message-wrapper').on('click', function(e) {
+  $('#message').hide();
+});
+
+$("#message-wrapper").on('mouseenter',function(e) {
+  $('#message').slideDown();
+});
+
+$("#message-wrapper").on('mouseleave',function(e) {
+  $('#message').hide();
+});
+
+//$('h1').on('click', function(e) {
+  //$(this).height(200);
+//});
