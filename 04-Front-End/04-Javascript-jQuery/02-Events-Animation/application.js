54503cfa885d836b7a665f0e5c8745feb2e1f9c5
Vincent Simon <vincent.simon@gadz.org>
2014-05-06 10:12:41 +0200

    correction

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,22 +3,55 @@
   * TODO: add your code here!
   *
 */
+$("#message").hide();
+$("#message").slideDown().delay(1500);
 
-$("#message-wrapper").hide();
-$("#message-wrapper").slideDown().delay(1500);
+//correction :
 
-$('#message-wrapper').on('click', function(e) {
+$(document).ready(function(){
+  $('#message').hide().delay(500).slideDown();
+});
+
+//----------------------------------------------------------------//
+
+$('#message').on('click', function(e) {
   $('#message').hide();
 });
+
+//correction :
+
+$(document).ready(function(){
+  $('#message').hide().delay(500).slideDown();
+  $('#message').on('click', function(){
+    $(this).slideUp()
+  })
+});
+
+//----------------------------------------------------------------//
 
 $("#message-wrapper").on('mouseenter',function(e) {
   $('#message').slideDown();
 });
 
+//correction :
+
+$("#message-wrapper").on('mouseenter',function(e) {
+  $('#message').slideDown();
+});
+
+//----------------------------------------------------------------//
+
 $("#message-wrapper").on('mouseleave',function(e) {
   $('#message').hide();
 });
 
+//correction :
+
+$("#message-wrapper").on('mouseleave',function(e) {
+  $('#message').slideUp();
+});
+
+//----------------------------------------------------------------//
 //$('h1').on('click', function(e) {
   //$(this).height(200);
 //});
