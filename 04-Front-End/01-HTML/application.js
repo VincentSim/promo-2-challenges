813135b3196094bfde0aa7ca1a55f9dcd192fb78
Vincent Simon <vincent.simon@gadz.org>
2014-05-06 19:01:47 +0200

    presque zaloeuil

--- 04-Front-End/01-HTML/application.js
+++ 04-Front-End/01-HTML/application.js
@@ -1,3 +1,25 @@
-$('img').on('click', function(e) {
-  height: "200px"
+$(document).ready(function(){
+    $('img').on('click', function(){
+    var $overlay = $('<div>').addClass('overlay'),
+        $body = $('body');
+
+    $overlay.hide().appendTo($body).fadeIn('slow');
+
+    var $photo = $('<img>').attr({
+      src:$(this).attr('src')
+    });
+
+    var $photoWrapper = $('<div>').addClass('photo-wrapper');
+    $photoWrapper.hide().append($photo).appendTo($body);
+    $photoWrapper.fadeIn('slow');
+
+
+
+    $photoWrapper.on('click', function(){
+      $photoWrapper.add($overlay).fadeOut('slow', function(){
+        $(this).remove();
+      });
+    })
+  })
+
 });
